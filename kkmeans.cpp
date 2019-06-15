62e026fcc29240cf37242f37b4e9e394c3a5104d
zazicam <dima.friends@mail.ru>
2019-06-15 20:19:04 +0300

    read data

--- kkmeans.cpp
+++ kkmeans.cpp
@@ -3,14 +3,22 @@
 #include <vector>
 
 #include <dlib/clustering.h>
-#include <dlib/rand.h>
 
-using namespace std;
 using namespace dlib;
 
-int main()
+int main(int argc, char **argv)
 {
-	cout<<"Hello! :)"<<endl;
+	if(argc!=2) {
+		std::cout<<"Error! Usage: "<<argv[0]<<" "<<"[number of clusters]"<<std::endl;
+		exit(1);
+	}
+
+	int n = atoi(argv[1]);
+	if(n<=1) {
+		std::cout<<"Error! Incorrect number '"<<argv[1]<<"'"<<std::endl;
+		std::cout<<"Need integer n >= 2"<<std::endl;
+		exit(1);
+	}
 
     typedef matrix<double,2,1> sample_type;
     typedef radial_basis_kernel<sample_type> kernel_type;
@@ -21,29 +29,20 @@ int main()
     std::vector<sample_type> samples;
     std::vector<sample_type> initial_centers;
 
+	// read data
     sample_type m;
-
-    dlib::rand rnd;
-
-    const long num = 200;
-
-    // make some samples near the origin
-    for (long i = 0; i < num; ++i)
-    {
-        m(0) = -100 + 200 * rnd.get_random_double();
-        m(1) = -100 + 200 * rnd.get_random_double();
-
-        samples.push_back(m);
-    }
+	while(std::cin>>m(0)>>m(1)) {
+		samples.push_back(m);
+	}
 
     test.set_number_of_centers(3);
     pick_initial_centers(3, initial_centers, samples, test.get_kernel());
 
     test.train(samples,initial_centers);
 
-	ofstream fout("kkmeans_ex.txt", ios::out);
+	std::ofstream fout("kkmeans_ex.txt", std::ios::out);
 	for(auto m : samples) {
-		fout<<m(0)<<"\t"<<m(1)<<"\t"<<test(m)<<endl;
+		fout<<m(0)<<" "<<m(1)<<std::endl;
 	}
 	fout.close();
 }
